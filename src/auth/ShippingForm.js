9cc427ae69255f2ea20d86cd8addc48f9b657f2b
Lucas Lambert <lucaslydon@Lucass-MacBook-Pro.local>
2021-10-11 14:16:25 -0400

    preparing for deployment

--- src/auth/ShippingForm.js
+++ src/auth/ShippingForm.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import Alert from "../common/Alert";
-import { Select ,CaretIcon, ModalCloseButton } from 'react-responsive-select';
-import 'react-responsive-select/dist/react-responsive-select.css';
+
 
 /** Shipping form to send coral.
  */
@@ -33,8 +32,10 @@ export default function ShippingForm() {
   async function handleSubmit(evt) {
     evt.preventDefault();
       history.push("/thank-you");
+    }
+  
     
-  }
+  
 
   /** Update form data field */
   function handleChange(evt) {
@@ -87,76 +88,6 @@ export default function ShippingForm() {
                       value={formData.zipCode}
                       onChange={handleChange}
                   />
-                </div>
-                <div className="form-group">
-                  <label>State</label>
-                  <Select
-                      name="carType1"
-                      className="form-control"
-                      modalCloseButton={<ModalCloseButton />}
-                      value={formData.state}
-                    //   onChange={handleChange}
-                  
-                      options={[
-                        { value:"AL", text:'Alabama' },
-                        { value:"AK", text:'Alaska' },
-                        { value:"AZ", text:'Arizona' },
-                        { value:"AR", text:'Arkansas' },
-                        { value:"CA", text:'California' },
-                        { value:"CO", text:'Colorado' },
-                        { value:"CT", text:'Connecticut' },
-                        { value:"DE", text:'Delaware' },
-                        { value:"DC", text:'District Of Columbia' },
-                        { value:"FL", text:'Florida' },
-                        { value:"GA", text:'Georgia' },
-                        { value:"HI", text:'Hawaii' },
-                        { value:"ID", text:'Idaho' },
-                        { value:"IL", text:'Illinois' },
-                        { value:"IN", text:'Indiana' },
-                        { value:"IA", text:'Iowa' },
-                        { value:"KS", text:'Kansas' },
-                        { value:"KY", text:'Kentucky' },
-                        { value:"LA", text:'Louisiana' },
-                        { value:"ME", text:'Maine' },
-                        { value:"MD", text:'Maryland' },
-                        { value:"MA", text:'Massachusetts' },
-                        { value:"MI", text:'Michigan' },
-                        { value:"MN", text:'Minnesota' },
-                        { value:"MS", text:'Mississippi' },
-                        { value:"MO", text:'Missouri' },
-                        { value:"MT", text:'Montana' },
-                        { value:"NE", text:'Nebraska' },
-                        { value:"NV", text:'Nevada' },
-                        { value:"NH", text:'New Hampshire' },
-                        { value:"NJ", text:'New Jersey' },
-                        { value:"NM", text:'New Mexico' },
-                        { value:"NY", text:'New York' },
-                        { value:"NC", text:'North Carolina' },
-                        { value:"ND", text:'North Dakota' },
-                        { value:"OH", text:'Ohio' },
-                        { value:"OK", text:'Oklahoma' },
-                        { value:"OR", text:'Oregon' },
-                        { value:"PA", text:'Pennsylvania' },
-                        { value:"RI", text:'Rhode Island' },
-                        { value:"SC", text:'South Carolina' },
-                        { value:"SD", text:'South Dakota' },
-                        { value:"TN", text:'Tennessee' },
-                        { value:"TX", text:'Texas' },
-                        { value:"UT", text:'Utah' },
-                        { value:"VT", text:'Vermont' },
-                        { value:"VA", text:'Virginia' },
-                        { value:"WA", text:'Washington' },
-                        { value:"WV", text:'West Virginia' },
-                        { value:"WI", text:'Wisconsin' },
-                        { value:"WY", text:'Wyoming' },
-                      ]}
-
-                      caretIcon={<CaretIcon />}
-                      selectedValue="Alabama"
-                      onChange={newValue => console.log('onChange', newValue)}
-                      onSubmit={() => console.log('onSubmit')}
-
-                    />
                 </div>
                 <div className="form-group">
                   <label>Email</label>
